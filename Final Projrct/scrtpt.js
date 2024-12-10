4646a076f600d0a4f09d8899493b4ae361d5aee2
MoazNasur <164564399+MoazNasur@users.noreply.github.com>
2024-12-09 22:55:13 -0500

    Update scrtpt.js

--- Final Projrct/scrtpt.js
+++ Final Projrct/scrtpt.js
@@ -1,4 +1,1 @@
-background-image: url(https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/Philadelphia_76ers_logo.svg/1200px-Philadelphia_76ers_logo.svg.png);
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
+
